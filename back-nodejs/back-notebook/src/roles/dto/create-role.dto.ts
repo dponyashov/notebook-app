8ad2aace31f50c21fc78768b2f3f86359bf8674c
dponyashov <ponyashov@gmail.com>
2026-05-13 11:54:39 +0400

    add validation

--- back-nodejs/back-notebook/src/roles/dto/create-role.dto.ts
+++ back-nodejs/back-notebook/src/roles/dto/create-role.dto.ts
@@ -1,10 +1,13 @@
 import { ApiProperty } from "@nestjs/swagger";
+import { IsString } from "class-validator";
 
 export class CreateRoleDto {
 
+    @IsString({ message: 'Должно быть строкой'})
     @ApiProperty({example: 'ADMIN', description: 'Значение роли пользователя'})
     readonly value: string;
 
+    @IsString({ message: 'Должно быть строкой'})
     @ApiProperty({example: 'Администратор', description: 'Описание роли пользователя'})
     readonly description: string;
 }
